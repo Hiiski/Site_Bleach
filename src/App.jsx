7f224990f3ff913c7bb377eb2d1a5b7871fb6967
EA - Henrique Soares Lima <henrique_soares@educadventista.org>
2024-04-08 10:06:54 -0300

    HAHAHAHHAHAHAHAHA

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Enredo from "./components/Enredo"
+import Header from "./components/Header"
+import Historia from "./components/Historia"
+
+export default function App() {
+  return (
+    <main className="bg-slate-400">
+      <div>
+        <Header />
+      </div>
+    </main>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <section>
+        <header>
+          <h1 className="text-5xl text-center">Bleach - Bankai</h1>
+        </header>
+    </section>
+  )
+}
